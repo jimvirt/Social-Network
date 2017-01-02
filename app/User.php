a0dbc627b3ae89f5f7a56c738aa6a48f981c5fdc
jimvirt <jimvirtucio@gmail.com>
2017-01-02 18:44:14 +0800

    Configured user

--- app/User.php
+++ app/User.php
@@ -2,25 +2,19 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
 
-class User extends Authenticatable
+class User extends Model implements Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    use \Illuminate\Auth\Authenticatable;
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function likes()
+    {
+        return $this->hasMany('App\Like');
+    }
 }
